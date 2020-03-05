2ef5312f0a7630d7fcc3f2e9a8f263bcc9d496b3
Ograll <stefan.klepikov@mail.ru>
2020-03-05 17:11:22 +0300

    COMPLETED_2_NUMBER

---
+++ web2/index.php
@@ -0,0 +1,26 @@
+<?php
+
+// Отправляем правильную кодировку.
+header('Content-Type: text/html; charset=UTF-8');
+
+// Выводим все полученные через POST параметры.
+// если запрос 2-5) сделан правильно, то можно будет увидеть
+// отправленный комментарий в ответе веб-сервера.
+print_r($_POST);
+
+print('Привет, мир!');
+?>
+
+<html>
+<head>
+</head>
+<body>
+	<br><img src="1.png">
+	<br><img src="2.png">
+	<br><img src="3.png">
+	<br><img src="4.png">
+	<br><img src="5.png">
+	<br><img src="6.png">
+	<br><img src="7.png">
+</body>
+</html>
