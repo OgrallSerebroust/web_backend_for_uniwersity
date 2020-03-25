1c207fb8094ac1559897b2c5e062d2f15f438166
Ograll <stefan.klepikov@mail.ru>
2020-03-25 23:55:35 +0300

    Created_checking_POST

--- web3/finding_data.php
+++ web3/finding_data.php
@@ -1,15 +1,18 @@
 <?
-  header("Content-Type: text/html; charset = UTF-8");
-  $database_url = "localhost";
+    header("Content-Type: text/html; charset = UTF-8");
+    $database_url = "localhost";
 	$database_login = "u16428";
 	$database_password = "Ogegal91";
 	$database_name = "u16428";
 	$connection = mysql_connect($database_url, $database_login, $database_password) or die(mysql_error());
 	$select_database = mysql_select_db($database_name, $connection) or die(mysql_error());
 
-  if((isset($_POST["name"])) && (isset($_POST["email"])) && (isset($_POST["birthday"])) && (isset($_POST["sex"])) && (isset($_POST["foots"])) && (isset($_POST["perks[]"])) && (isset($_POST["confirm"])))
-  {
-
-  }
-  #&& (isset($_POST["email"])) && (isset($_POST["birthday"])) && (isset($_POST["sex"])) && (isset($_POST["foots"])) && (isset($_POST["perks"])) && (isset($_POST[""])) && (isset($_POST["confirm"])))
+    if((isset($_POST["name"])) && (isset($_POST["email"])) && (isset($_POST["birthday"])) && (isset($_POST["sex"])) && (isset($_POST["foots"])) && (isset($_POST["perks"])) && (isset($_POST["biographi"])) && (isset($_POST["checking_verify"])) && (isset($_POST["confirm"])))
+    {
+        echo "Good!";
+    }
+    else
+    {
+        echo "bad";
+    }
 ?>
