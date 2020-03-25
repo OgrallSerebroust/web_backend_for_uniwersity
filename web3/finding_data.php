995f203e08af1de4af047bae2432c2a016e3249d
Ograll <stefan.klepikov@mail.ru>
2020-03-26 01:02:41 +0300

    Trying

--- web3/finding_data.php
+++ web3/finding_data.php
@@ -1,11 +1,3 @@
 <?
     echo '<a href = "#modal">Регистрация</a>';
-    #if((isset($_POST["name"])) && (isset($_POST["email"])) && (isset($_POST["birthday"])) && (isset($_POST["sex"])) && (isset($_POST["foots"])) && (isset($_POST["perks"])) && (isset($_POST["biographi"])) && (isset($_POST["checking_verify"])) && (isset($_POST["confirm"])))
-    #{
-        #echo "Good!";
-    #}
-    #else
-    #{
-       # echo "bad";
-    #}
 ?>
