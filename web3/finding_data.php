5d1a3f359d90ae457f18b8b295bc4cf073a5e775
Ograll <stefan.klepikov@mail.ru>
2020-03-26 01:35:44 +0300

    Debugging

--- web3/finding_data.php
+++ web3/finding_data.php
@@ -2,10 +2,10 @@
     include("include/settings.php");
     if((isset($_POST["name"])) && (isset($_POST["email"])) && (isset($_POST["birthday"])) && (isset($_POST["sex"])) && (isset($_POST["foots"])) && (isset($_POST["perks"])) && (isset($_POST["biographi"])) && (isset($_POST["checking_verify"])) && (isset($_POST["confirm"])))
     {
-        #echo "Good!";
+        echo "Good!";
     }
     else
     {
-       # echo "bad";
+        echo "bad";
     }
 ?>
