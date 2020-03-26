9c7e7ae01a6a9ee2632ca8099c53fce61d6233a5
Ograll <stefan.klepikov@mail.ru>
2020-03-27 00:36:38 +0300

    Back_into_the_future

--- web3/finding_data.php
+++ web3/finding_data.php
@@ -9,37 +9,34 @@
         foreach($_POST["perks"] as $j => $number_of_perk_in_the_flow) $array_of_perks[$j] = $number_of_perk_in_the_flow;
 
         $good_type_of_perks_for_database = implode(", ", $array_of_perks);
+        echo $_POST["checking_verify"];
+        echo $_POST["confirm"];
         mysqli_query($connection, "INSERT INTO for_number_3(name, email, birthday, sex, foots, perks, biographi) VALUES('$name', '$email', '".$_POST["birthday"]."', '".$_POST["sex"]."', '".$_POST["foots"]."', '$good_type_of_perks_for_database', '".$_POST["biographi"]."')");
-        echo "Good";
+        mysqli_close($connection);
+        echo "Good!";
     }
     else if($_POST["name"] == '')
     {
         echo "Ошибка! Уважаемый пользователь, вы не ввели имя...";
-        mysqli_close($connection);
     }
     else if($_POST["email"] == '')
     {
         echo "Ошибка! Уважаемый пользователь, вы не ввели почту...";
-        mysqli_close($connection);
     }
     else if($_POST["birthday"] == "Год")
     {
         echo "Ошибка! Уважаемый пользователь, вы не выбрали год рождения...";
-        mysqli_close($connection);
     }
     else if($_POST["biographi"] == '')
     {
         echo "Ошибка! Уважаемый пользователь, вы не рассказали ничего о себе...";
-        mysqli_close($connection);
     }
     else if(!isset($_POST["checking_verify"]))
     {
-        echo "Ошибка! Уважаемый пользователь, вы не подтвердили ознакомление с контрактом...";
-        mysqli_close($connection);
+        echo "Ошибка! Уважаемый пользователь, вы не подтвердили ознакомление с контрактом!";
     }
     else
     {
         echo "Ошибка! Уважаемый пользователь, вы не заполнили одно или несколько полей...";
-        mysqli_close($connection);
     }
 ?>
