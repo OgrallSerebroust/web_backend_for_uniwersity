7dc41ddc189ebbf1b7eeaa157c20ef5dd06d5e1a
Ograll <stefan.klepikov@mail.ru>
2020-03-26 22:10:20 +0300

    Debuging

--- web3/finding_data.php
+++ web3/finding_data.php
@@ -7,7 +7,7 @@
             $name = htmlspecialchars($_POST["name"]);
             $email = htmlspecialchars($_POST["email"]);
             echo $_POST["name"];
-            echo $_POST["sex"]
+            echo $_POST["sex"];
             mysqli_query($connection, "INSERT INTO for_number_3(name, email, birthday, sex, foots) VALUES('$name', '$email', '".$_POST["birthday"]."', '".$_POST["sex"]."', '".$_POST["foots"]."')");
             mysqli_close($connection);
         }
