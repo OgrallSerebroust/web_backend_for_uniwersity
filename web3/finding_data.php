fc6d6ba9a046a360d49e5486827ec4f63da42664
Ograll <stefan.klepikov@mail.ru>
2020-03-26 23:25:17 +0300

    Completed_addition_of_biographi

--- web3/finding_data.php
+++ web3/finding_data.php
@@ -6,7 +6,7 @@
         $email = htmlspecialchars($_POST["email"]);
         echo $_POST["biographi"];
         echo $_POST["name"];
-        mysqli_query($connection, "INSERT INTO for_number_3(name, email, birthday, sex, foots) VALUES('$name', '$email', '".$_POST["birthday"]."', '".$_POST["sex"]."', '".$_POST["foots"]."')");
+        mysqli_query($connection, "INSERT INTO for_number_3(name, email, birthday, sex, foots, biographi) VALUES('$name', '$email', '".$_POST["birthday"]."', '".$_POST["sex"]."', '".$_POST["foots"]."', '".$_POST["biographi"]."')");
         mysqli_close($connection);
         echo "Good!";
     }
@@ -21,6 +21,10 @@
     else if($_POST["birthday"] == "Год")
     {
         echo "Ошибка! Уважаемый пользователь, вы не выбрали год рождения...";
+    }
+    else if($_POST["biographi"] == '')
+    {
+        echo "Ошибка! Уважаемый пользователь, вы не рассказали ничего о себе...";
     }
     else
     {
