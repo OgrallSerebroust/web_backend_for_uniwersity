ff41c43908713ff5067016b1421a560f8f33c0eb
Ograll <stefan.klepikov@mail.ru>
2020-03-27 00:33:07 +0300

    Debuging

--- web3/finding_data.php
+++ web3/finding_data.php
@@ -10,6 +10,7 @@
 
         $good_type_of_perks_for_database = implode(", ", $array_of_perks);
         mysqli_query($connection, "INSERT INTO for_number_3(name, email, birthday, sex, foots, perks, biographi) VALUES('$name', '$email', '".$_POST["birthday"]."', '".$_POST["sex"]."', '".$_POST["foots"]."', '$good_type_of_perks_for_database', '".$_POST["biographi"]."')");
+        header('Location: ?save=1');
     }
     else if($_POST["name"] == '')
     {
