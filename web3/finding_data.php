3f77cdfab3aadb08d54bf6e146d2bcc6620197cb
Ograll <stefan.klepikov@mail.ru>
2020-03-26 22:32:58 +0300

    Added_checking_of_name

--- web3/finding_data.php
+++ web3/finding_data.php
@@ -2,24 +2,20 @@
     include("include/settings.php");
     if((isset($_POST["name"])) && (isset($_POST["email"])) && (isset($_POST["birthday"])) && (isset($_POST["sex"])) && (isset($_POST["foots"])) && (isset($_POST["perks"])) && (isset($_POST["biographi"])) && (isset($_POST["checking_verify"])) && (isset($_POST["confirm"])))
     {
-        try
-        {
-            $name = htmlspecialchars($_POST["name"]);
-            $email = htmlspecialchars($_POST["email"]);
-            echo $_POST["name"];
-            echo $_POST["email"];
-            echo $_POST["birthday"];
-            echo $_POST["sex"];
-            echo $_POST["foots"];
-            mysqli_query($connection, "INSERT INTO for_number_3(name, email, birthday, sex, foots) VALUES('$name', '$email', '".$_POST["birthday"]."', '".$_POST["sex"]."', '".$_POST["foots"]."')");
-            mysqli_close($connection);
-        }
-        catch(PDOException $e)
-        {
-            print('Error : ' . $e->getMessage());
-            exit();
-        }
+        $name = htmlspecialchars($_POST["name"]);
+        $email = htmlspecialchars($_POST["email"]);
+        echo $_POST["name"];
+        echo $_POST["email"];
+        echo $_POST["birthday"];
+        echo $_POST["sex"];
+        echo $_POST["foots"];
+        mysqli_query($connection, "INSERT INTO for_number_3(name, email, birthday, sex, foots) VALUES('$name', '$email', '".$_POST["birthday"]."', '".$_POST["sex"]."', '".$_POST["foots"]."')");
+        mysqli_close($connection);
         echo "Good!";
+    }
+    else if(!isset($_POST["name"]))
+    {
+        echo "Ошибка! Уважаемый пользователь, вы не ввели имя...";
     }
     else
     {
