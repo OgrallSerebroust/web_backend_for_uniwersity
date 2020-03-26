88967c0a26e0cf586e41fc9533339763836bfc20
Ograll <stefan.klepikov@mail.ru>
2020-03-26 15:11:26 +0300

    Debuging

--- web3/include/settings.php
+++ web3/include/settings.php
@@ -6,5 +6,12 @@
 	$database_password = "TB5ENmFh";
 	$database_name = "u16428";
 	$connection = mysqli_connect($database_url, $database_login, $database_password) or die(mysql_error());
+    if (!$link)
+    {
+        echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+        echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
 	$select_database = mysql_select_db($database_name, $connection) or die(mysql_error());
 ?>
