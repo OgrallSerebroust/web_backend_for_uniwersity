6e5aae211648bc4893cba9f08a0e4bc4ebe3eea0
Ograll <stefan.klepikov@mail.ru>
2020-03-26 14:45:03 +0300

    Added_true_password

--- web3/include/settings.php
+++ web3/include/settings.php
@@ -3,7 +3,7 @@
 
     $database_url = "localhost";
 	$database_login = "u16428";
-	$database_password = "Ogegal91";
+	$database_password = "TB5ENmFh";
 	$database_name = "u16428";
 	$connection = mysqli_connect($database_url, $database_login, $database_password) or die(mysql_error());
 	$select_database = mysql_select_db($database_name, $connection) or die(mysql_error());
