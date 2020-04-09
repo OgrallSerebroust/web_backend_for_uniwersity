f6c532f9296533f3c70ed87ec950f2b9b92072a2
Ograll <stefan.klepikov@mail.ru>
2020-04-09 21:44:40 +0300

    Added_folder_for_number_4

---
+++ web4/finding_data.php
@@ -0,0 +1,25 @@
+<?php
+    include("include/settings.php");
+
+    if(($_POST["name"] != '') && ($_POST["email"] != '') && ($_POST["birthday"] != 'Год') && (isset($_POST["sex"])) && (isset($_POST["foots"])) && ($_POST["biographi"] != '') && (isset($_POST["checking_verify"])) && ($_POST["confirm"]) == "Confirm")
+    {
+        $name = htmlspecialchars($_POST["name"]);
+        $email = htmlspecialchars($_POST["email"]);
+        $array_of_perks = array();
+
+        foreach($_POST["perks"] as $j => $number_of_perk_in_the_flow) $array_of_perks[$j] = $number_of_perk_in_the_flow;
+
+        $good_type_of_perks_for_database = implode(", ", $array_of_perks);
+        mysqli_query($connection, "INSERT INTO for_number_3(name, email, birthday, sex, foots, perks, biographi) VALUES('$name', '$email', '".$_POST["birthday"]."', '".$_POST["sex"]."', '".$_POST["foots"]."', '$good_type_of_perks_for_database', '".$_POST["biographi"]."')");
+        mysqli_close($connection);
+        header('Location: index.php?completed_registration=1');
+    }
+    else if($_POST["name"] == '') echo "Ошибка! Уважаемый пользователь, вы не ввели имя...";
+    else if($_POST["email"] == '') echo "Ошибка! Уважаемый пользователь, вы не ввели почту...";
+    else if($_POST["birthday"] == "Год") echo "Ошибка! Уважаемый пользователь, вы не выбрали год рождения...";
+    else if($_POST["biographi"] == '') echo "Ошибка! Уважаемый пользователь, вы не рассказали ничего о себе...";
+    else if(!isset($_POST["checking_verify"])) echo "Ошибка! Уважаемый пользователь, вы не подтвердили ознакомление с контрактом!";
+    else echo "Ошибка! Уважаемый пользователь, вы не заполнили одно или несколько полей...";
+
+    mysqli_close($connection);
+?>
---
+++ web4/index.php
@@ -0,0 +1,135 @@
+<?
+    include("include/settings.php");
+?>
+<!DOCTYPE html>
+<html>
+
+<head lang="ru">
+    <title>
+        Форма для Веб-сервера
+    </title>
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div id="our_main_shadow"></div>
+    <?php
+        if(!empty($_GET['completed_registration'])) echo '<div>Уважаемый пользователь! Поздравляем, все данные сохранены!</div>';
+    ?>
+    <div id="our_site">
+        <form method="post" action="finding_data.php">
+            <table width="50%" height="500">
+                <tr>
+                    <th>
+                        Имя:
+                    </th>
+                    <td colspan="8">
+                        <input class="our_text_inputs_in_table" name="name" size="40" maxlength="40" type="text">
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        E-mail:
+                    </th>
+                    <td colspan="8">
+                        <input class="our_text_inputs_in_table" name="email" size="40" maxlength="40" type="text">
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        Дата рождения:
+                    </th>
+                    <td colspan="8">
+                        <select class="selections_in_our_table" name="birthday">
+                            <option selected>Год</option>
+                            <?php
+                                for($i = 1900; $i <= 2020; $i++) echo '<option>' . $i . '</option>';
+                            ?>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        Пол:
+                    </th>
+                    <td colspan="2">
+                        Мужской:
+                    </td>
+                    <td colspan="2">
+                        <input name="sex" type="radio" value="Man">
+                    </td>
+                    <td colspan="2">
+                        Женский:
+                    </td>
+                    <td colspan="2">
+                        <input name="sex" type="radio" value="Woman">
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        Колличество конечностей:
+                    </th>
+                    <td>
+                        2
+                    </td>
+                    <td>
+                        <input name="foots" type="radio" value="2">
+                    </td>
+                    <td>
+                        4
+                    </td>
+                    <td>
+                        <input name="foots" type="radio" value="4">
+                    </td>
+                    <td>
+                        6
+                    </td>
+                    <td>
+                        <input name="foots" type="radio" value="6">
+                    </td>
+                    <td>
+                        8
+                    </td>
+                    <td>
+                        <input name="foots" type="radio" value="8">
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        Сверхспособности:
+                    </th>
+                    <td colspan="8">
+                        <select class="selections_in_our_table" multiple size="3" name="perks[]">
+                            <option value="Immortality" selected>Бессмертие
+                            <option value="Passing">Прохождение сквозь стены
+                            <option value="Levitation">Левитация
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        Биография:
+                    </th>
+                    <td colspan="8">
+                        <textarea id="biographi_are_in_table" name="biographi" placeholder="Главная информация о вас..."></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        С контрактом ознакомлен!
+                    </th>
+                    <td colspan="8">
+                        <input name="checking_verify" type="checkbox">
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="9">
+                        <input id="button_of_confirming_in_our_table" name="confirm" type="submit" value="Confirm">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+</body>
+
+</html>
