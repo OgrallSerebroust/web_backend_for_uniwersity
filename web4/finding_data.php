21cddfbba94e264e07dcea60cb2b4a0ac22935c9
Ograll <stefan.klepikov@mail.ru>
2020-04-09 23:12:33 +0300

    Debugging

--- web4/finding_data.php
+++ web4/finding_data.php
@@ -12,7 +12,7 @@
         $good_type_of_perks_for_database = implode(", ", $array_of_perks);
         mysqli_query($connection, "INSERT INTO for_number_3(name, email, birthday, sex, foots, perks, biographi) VALUES('$name', '$email', '".$_POST["birthday"]."', '".$_POST["sex"]."', '".$_POST["foots"]."', '$good_type_of_perks_for_database', '".$_POST["biographi"]."')");
         mysqli_close($connection);
-        setcookie('saved', '1');
+        setcookie('saved', 'true');
         #header('Location: index.php?completed_registration=1');
         header('Location: index.php');
     }
