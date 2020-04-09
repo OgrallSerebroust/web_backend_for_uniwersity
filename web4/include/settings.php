88f30df49799a6c8fd64e326302c57e7af824a39
Ograll <stefan.klepikov@mail.ru>
2020-04-09 22:26:07 +0300

    Added_cookie

--- web4/include/settings.php
+++ web4/include/settings.php
@@ -6,4 +6,22 @@
 	$database_password = "6878969";
 	$database_name = "u16428";
 	$connection = mysqli_connect($database_url, $database_login, $database_password, $database_name) or die();
+    $messages = array();
+    $errors = array();
+    $values = array();
+    if ($_SERVER['REQUEST_METHOD'] == 'GET')
+    {
+        if(!empty($_COOKIE['saved']))
+        {
+            setcookie('saved', '');
+            $messages[] = 'Спасибо, результаты сохранены.';
+        }
+        if ($errors['name'])
+        {
+            setcookie('error_of_name', '');
+            $messages[] = '<div class="error">Заполните имя.</div>';
+        }
+
+        $values['name'] = empty($_COOKIE['value_of_name']) ? '' : $_COOKIE['value_of_name'];
+    }
 ?>
