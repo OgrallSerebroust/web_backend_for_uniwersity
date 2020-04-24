0c07ba973b6cb80bcc9b3951fc2c8f9dd0073c25
Ograll <stefan.klepikov@mail.ru>
2020-04-24 23:24:41 +0300

    debudding

--- web5/login.php
+++ web5/login.php
@@ -33,7 +33,6 @@
     {
         $query_for_cheking_trulity = mysqli_query($connection, "SELECT * FROM for_number_3");
         //WHERE login = ".$_POST["login"]."
-        echo $query_for_cheking_trulity;
         $row_with_query_for_cheking_trulity = mysqli_fetch_array($query_for_cheking_trulity);
         echo $query_for_cheking_trulity["id"];
         session_start();
