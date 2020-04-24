83b3c77c8759a8b76f3e087fcb04faad190bac43
Ograll <stefan.klepikov@mail.ru>
2020-04-24 23:29:21 +0300

    Trying

--- web5/login.php
+++ web5/login.php
@@ -33,7 +33,7 @@
     {
         $query_for_cheking_trulity = mysqli_query($connection, "SELECT * FROM for_number_3");
         //WHERE login = ".$_POST["login"]."
-        $row_with_query_for_cheking_trulity = mysqli_fetch_array($query_for_cheking_trulity);
+        $row_with_query_for_cheking_trulity = $row = $query_for_cheking_trulity->fetch_array()
         echo $query_for_cheking_trulity["id"];
         session_start();
         $_SESSION["login"] = $_POST["login"];
