7d578d3657abb79403ec114aa89a2b1a08fb73ee
Ograll <stefan.klepikov@mail.ru>
2020-04-25 01:45:48 +0300

    Trying

--- web5/login.php
+++ web5/login.php
@@ -31,7 +31,8 @@
 
     if($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        $query_for_cheking_trulity = mysqli_query($connection, "SELECT * FROM for_number_3 WHERE login = ".$_POST['login']."");
+        $php_sosi_ja_tiebia_obmanul = $_POST['login']
+        $query_for_cheking_trulity = mysqli_query($connection, "SELECT * FROM for_number_3 WHERE login = $php_sosi_ja_tiebia_obmanul");
         echo $query_for_cheking_trulity;
         $row_with_query_for_cheking_trulity = mysqli_fetch_array($query_for_cheking_trulity);
         session_start();
