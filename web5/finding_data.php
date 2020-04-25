7a5b9ef3fa39c7c27b51a715f181aa769408e559
Ograll <stefan.klepikov@mail.ru>
2020-04-25 04:21:24 +0300

    Trying

--- web5/finding_data.php
+++ web5/finding_data.php
@@ -87,12 +87,11 @@
 
 
 
-        if(empty($errors) && (isset($_COOKIE[session_name()])) && session_start() && (isset($_SESSION["login"])))
+        if(empty($errors) && (isset($_SESSION["login"])))
         {
             $query_for_loading_users_information = mysqli_query($connection, "SELECT * FROM for_number_3 WHERE login = '".$_SESSION["login"]."'");
             $row_with_query_for_loading_users_information = mysqli_fetch_array($query_for_loading_users_information);
             $values["name"] = $row_with_query_for_loading_users_information["name"];
-            printf("%s", $_SESSION["login"]);
         }
 
         include('our_site.php');
