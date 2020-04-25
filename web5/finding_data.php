6f82cc6aace44918376821643b90531d327a2bae
Ograll <stefan.klepikov@mail.ru>
2020-04-25 03:56:32 +0300

    Checking

--- web5/finding_data.php
+++ web5/finding_data.php
@@ -90,6 +90,7 @@
             $query_for_loading_users_information = mysqli_query($connection, "SELECT * FROM for_number_3 WHERE login = '".$_SESSION["login"]."'");
             $row_with_query_for_loading_users_information = mysqli_fetch_array($query_for_loading_users_information);
             $values["name"] = $row_with_query_for_loading_users_information["name"];
+            printf("%s"), $values["name"];
         }
 
         include('our_site.php');
