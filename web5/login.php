577d2f20e0ad3dd60152081a5590ae244a67ca2f
Ograll <stefan.klepikov@mail.ru>
2020-04-25 04:49:34 +0300

    trying

--- web5/login.php
+++ web5/login.php
@@ -42,7 +42,7 @@
             $_SESSION["password"] = $obman_php_second;
             $_SESSION['uid'] = 3141592;
             session_start();
-            header("Location: ./");
+            include('finding_data.php');
         }
         else
         {
