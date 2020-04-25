cb250f9133d97d45d65f99a0911461242ced5240
Ograll <stefan.klepikov@mail.ru>
2020-04-25 05:19:14 +0300

    Debugging

--- web5/finding_data.php
+++ web5/finding_data.php
@@ -87,11 +87,12 @@
 
         if(empty($errors) && (isset($_SESSION["login"])))
         {
+            printf('Вход с логином %s, uid %d', $_SESSION['login'], $_SESSION['uid']);
             $query_for_loading_users_information = mysqli_query($connection, "SELECT * FROM for_number_3 WHERE login = '".$_SESSION["login"]."'");
             $row_with_query_for_loading_users_information = mysqli_fetch_array($query_for_loading_users_information);
             $values["name"] = $row_with_query_for_loading_users_information["name"];
         }
-        if(isset($_SESSION['uid'])) printf('Вход с логином %s, uid %d', $_SESSION['login'], $_SESSION['uid']);
+
         include('our_site.php');
     }
     else
