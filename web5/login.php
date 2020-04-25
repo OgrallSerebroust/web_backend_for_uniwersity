7e66e4c6f7474c85a5b082467d5538b9a824e61c
Ograll <stefan.klepikov@mail.ru>
2020-04-25 04:44:52 +0300

    Trying

--- web5/login.php
+++ web5/login.php
@@ -42,7 +42,7 @@
             $_SESSION["password"] = $obman_php_second;
             $_SESSION['uid'] = 3141592;
             session_start();
-            header("Location: /web_backend_for_uniwersity/web5/finding_data.php");
+            header("Location: ./");
         }
         else
         {
