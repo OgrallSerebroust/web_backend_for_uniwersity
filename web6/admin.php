c91d69c696bc12be9d94298a0516e535b78b18c6
Ograll <stefan.klepikov@mail.ru>
2020-05-07 20:13:59 +0300

    Debugging

--- web6/admin.php
+++ web6/admin.php
@@ -1,5 +1,5 @@
 <?php
-    if((empty($_SERVER["PHP_AUTH_USER"])) || (empty($_SERVER["PHP_AUTH_PW"])))
+    if((empty($_SERVER["PHP_AUTH_USER"])) || (empty($_SERVER["PHP_AUTH_PW"])) || ($_SERVER["PHP_AUTH_USER"] != "admin"))
     {
         header("WWW-Authenticate: Basic realm='My realm'");
         header("HTTP/1.1 401 Unauthorized");
