08601563a548a59e7510c99dba7cf732f9c7eb0b
Ograll <stefan.klepikov@mail.ru>
2020-05-07 22:11:44 +0300

    Added_showing

--- web6/admin.php
+++ web6/admin.php
@@ -9,31 +9,5 @@
     else
     {
         print("Вы успешно авторизовались и видите защищённые паролем данные.");
-        $database_url = "localhost";
-        $database_login = "u16428";
-        $database_password = "6878969";
-        $database_name = "u16428";
-        $connection = mysqli_connect($database_url, $database_login, $database_password, $database_name) or die();
-        $query_with_all_information = mysqli_query($connection, "SELECT * FROM for_number_3");
-        $array_with_query_with_all_information = mysqli_fetch_array($query_with_all_information);?>
-<html>
-    <table>
-        <tbody>
-            <tr>
-                <th>
-                    Имя пользователя
-                </th>
-                <th>
-                    Пароль
-                </th>
-            </tr>
-            <?php
-                do
-                {
-                    echo "<tr><td>".$array_with_query_with_all_information['name']."</td><td>".$array_with_query_with_all_information['password']."</td></tr>";
-                }
-                while($array_with_query_with_all_information = mysqli_fetch_array($query_with_all_information));?>
-        </tbody>
-    </table>
-</html>
-<?php}?>
+        include("table.php");
+    }
---
+++ web6/table.php
@@ -0,0 +1,29 @@
+<?php
+    $database_url = "localhost";
+    $database_login = "u16428";
+    $database_password = "6878969";
+    $database_name = "u16428";
+    $connection = mysqli_connect($database_url, $database_login, $database_password, $database_name) or die();
+    $query_with_all_information = mysqli_query($connection, "SELECT * FROM for_number_3");
+    $array_with_query_with_all_information = mysqli_fetch_array($query_with_all_information);?>
+
+<html>
+    <table>
+        <tbody>
+            <tr>
+                <th>
+                    Имя пользователя
+                </th>
+                <th>
+                    Пароль
+                </th>
+            </tr>
+            <?php
+                do
+                {
+                    echo "<tr><td>".$array_with_query_with_all_information['name']."</td><td>".$array_with_query_with_all_information['password']."</td></tr>";
+                }
+                while($array_with_query_with_all_information = mysqli_fetch_array($query_with_all_information));?>
+        </tbody>
+    </table>
+</html>
