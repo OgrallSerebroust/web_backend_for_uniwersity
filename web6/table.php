026db51724fb73a409c142efe5ba56b6df8b37b8
Ograll <stefan.klepikov@mail.ru>
2020-05-07 22:57:19 +0300

    Added_style_of_showing

--- web6/table.php
+++ web6/table.php
@@ -15,47 +15,49 @@
         <link href="css/style.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        <table>
-            <tbody>
-            <tr>
-                <th>
-                    ID пользователя
-                </th>
-                <th>
-                    Имя пользователя
-                </th>
-                <th>
-                    Логин
-                </th>
-                <th>
-                    Пароль
-                </th>
-                <th>
-                    Почта
-                </th>
-                <th>
-                    Дата рождения
-                </th>
-                <th>
-                    Пол
-                </th>
-                <th>
-                    Количество конечностей
-                </th>
-                <th>
-                    Умения
-                </th>
-                <th>
-                    Биография
-                </th>
-            </tr>
-            <?php
-            do
-            {
-                echo "<tr><td>".$array_with_query_with_all_information['id']."</td><td>".$array_with_query_with_all_information['name']."</td><td>".$array_with_query_with_all_information['login']."</td><td>".$array_with_query_with_all_information['password']."</td><td>".$array_with_query_with_all_information['email']."</td><td>".$array_with_query_with_all_information['birthday']."</td><td>".$array_with_query_with_all_information['sex']."</td><td>".$array_with_query_with_all_information['foots']."</td><td>".$array_with_query_with_all_information['perks']."</td><td>".$array_with_query_with_all_information['biographi']."</td></tr>";
-            }
-            while($array_with_query_with_all_information = mysqli_fetch_array($query_with_all_information));?>
-            </tbody>
-        </table>
+        <div id="main_part_of_our_site">
+            <table>
+                <tbody>
+                    <tr>
+                        <th>
+                            ID пользователя
+                        </th>
+                        <th>
+                            Имя пользователя
+                        </th>
+                        <th>
+                            Логин
+                        </th>
+                        <th>
+                            Пароль
+                        </th>
+                        <th>
+                            Почта
+                        </th>
+                        <th>
+                            Дата рождения
+                        </th>
+                        <th>
+                            Пол
+                        </th>
+                        <th>
+                            Количество конечностей
+                        </th>
+                        <th>
+                            Умения
+                        </th>
+                        <th>
+                            Биография
+                        </th>
+                    </tr>
+                    <?php
+                        do
+                        {
+                            echo "<tr><td>".$array_with_query_with_all_information['id']."</td><td>".$array_with_query_with_all_information['name']."</td><td>".$array_with_query_with_all_information['login']."</td><td>".$array_with_query_with_all_information['password']."</td><td>".$array_with_query_with_all_information['email']."</td><td>".$array_with_query_with_all_information['birthday']."</td><td>".$array_with_query_with_all_information['sex']."</td><td>".$array_with_query_with_all_information['foots']."</td><td>".$array_with_query_with_all_information['perks']."</td><td>".$array_with_query_with_all_information['biographi']."</td></tr>";
+                        }
+                        while($array_with_query_with_all_information = mysqli_fetch_array($query_with_all_information));?>
+                </tbody>
+            </table>
+        </div>
     </body>
 </html>
