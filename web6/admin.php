29bd76ee1b2e7088d682b445628316de7e27bc65
Ograll <stefan.klepikov@mail.ru>
2020-05-07 20:07:01 +0300

    Debugging

--- web6/admin.php
+++ web6/admin.php
@@ -1,11 +1,13 @@
 <?php
     if((empty($_SERVER["PHP_AUTH_USER"])) || (empty($_SERVER["PHP_AUTH_PW"])) || ($_SERVER["PHP_AUTH_USER"] != "admin") || (md5($_SERVER["PHP_AUTH_PW"] != md5("123"))))
     {
-        header("HTTP/1.1 401 Unauthorized");
         header("WWW-Authenticate: Basic realm='My realm'");
+        header("HTTP/1.1 401 Unauthorized");
         print("<h1>401 Требуется авторизация</h1>");
         exit();
     }
-
-    print("Вы успешно авторизовались и видите защищённые паролем данные.");
+    else
+    {
+        print("Вы успешно авторизовались и видите защищённые паролем данные.");
+    }
     ?>
