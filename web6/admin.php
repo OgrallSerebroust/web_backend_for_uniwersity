a6f12d726412d8216df736e45c6c42aae908936f
Ograll <stefan.klepikov@mail.ru>
2020-05-07 21:06:37 +0300

    Added_showing

--- web6/admin.php
+++ web6/admin.php
@@ -15,10 +15,25 @@
         $database_name = "u16428";
         $connection = mysqli_connect($database_url, $database_login, $database_password, $database_name) or die();
         $query_with_all_information = mysqli_query($connection, "SELECT * FROM for_number_3");
-        $array_with_query_with_all_information = mysqli_fetch_array($query_with_all_information);
-        do
-        {
-            echo $array_with_query_with_all_information["name"];
-        }
-        while($array_with_query_with_all_information = mysqli_fetch_array($query_with_all_information));
-    }
+        $array_with_query_with_all_information = mysqli_fetch_array($query_with_all_information);?>
+<html>
+    <table>
+        <tbody>
+            <tr>
+                <th>
+                    Имя пользователя
+                </th>
+                <th>
+                    Пароль
+                </th>
+            </tr>
+            <?php
+                do
+                {
+                    echo "<tr><td>".$array_with_query_with_all_information['name']."</td><td>".$array_with_query_with_all_information['password']."</td></tr>";
+                }
+                while($array_with_query_with_all_information = mysqli_fetch_array($query_with_all_information));?>
+        </tbody>
+    </table>
+</html>
+<?php}?>
