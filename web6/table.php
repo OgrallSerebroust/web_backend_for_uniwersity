112890215ead93b25c0f888fb8f76d3454f142b4
Ograll <stefan.klepikov@mail.ru>
2020-05-08 01:39:18 +0300

    Added_style_of_showing

--- web6/table.php
+++ web6/table.php
@@ -5,7 +5,12 @@
     $database_name = "u16428";
     $connection = mysqli_connect($database_url, $database_login, $database_password, $database_name) or die();
     $query_with_all_information = mysqli_query($connection, "SELECT * FROM for_number_3");
-    $array_with_query_with_all_information = mysqli_fetch_array($query_with_all_information);?>
+    $array_with_query_with_all_information = mysqli_fetch_array($query_with_all_information);
+
+    if(isset($_POST["delete"]))
+    {
+        echo"Good!";
+    }?>
 
 <html>
     <head>
@@ -52,11 +57,14 @@
                         <th>
                             Биография
                         </th>
+                        <th>
+                            Удаление
+                        </th>
                     </tr>
                     <?php
                     do
                     {
-                        echo "<tr><td>".$array_with_query_with_all_information['id']."</td><td>".$array_with_query_with_all_information['name']."</td><td>".$array_with_query_with_all_information['login']."</td><td>".$array_with_query_with_all_information['password']."</td><td>".$array_with_query_with_all_information['email']."</td><td>".$array_with_query_with_all_information['birthday']."</td><td>".$array_with_query_with_all_information['sex']."</td><td>".$array_with_query_with_all_information['foots']."</td><td>".$array_with_query_with_all_information['perks']."</td><td>".$array_with_query_with_all_information['biographi']."</td><td><input type='submit' value='Удалить'></td></tr>";
+                        echo "<tr><td>".$array_with_query_with_all_information['id']."</td><td>".$array_with_query_with_all_information['name']."</td><td>".$array_with_query_with_all_information['login']."</td><td>".$array_with_query_with_all_information['password']."</td><td>".$array_with_query_with_all_information['email']."</td><td>".$array_with_query_with_all_information['birthday']."</td><td>".$array_with_query_with_all_information['sex']."</td><td>".$array_with_query_with_all_information['foots']."</td><td>".$array_with_query_with_all_information['perks']."</td><td>".$array_with_query_with_all_information['biographi']."</td><td><input type='submit' name = 'delete' value='Удалить'></td></tr>";
                     }
                     while($array_with_query_with_all_information = mysqli_fetch_array($query_with_all_information));?>
                     </tbody>
