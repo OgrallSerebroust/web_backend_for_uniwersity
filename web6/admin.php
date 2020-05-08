889eeba410caed45f1586ac8a90b5fcab7fe67d2
Ograll <stefan.klepikov@mail.ru>
2020-05-08 05:40:21 +0300

    Debugging

--- web6/admin.php
+++ web6/admin.php
@@ -1,7 +1,5 @@
 <?php
     include("settings.php");
-    $query_for_admin_entering = mysqli_query($connection, "SELECT * FROM admin_info");
-    $array_with_query_for_admin_entering = mysqli_fetch_array($query_for_admin_entering);
     if((empty($_SERVER["PHP_AUTH_USER"])) || (empty($_SERVER["PHP_AUTH_PW"])) || ($_SERVER["PHP_AUTH_USER"] != "admin") || (md5($_SERVER["PHP_AUTH_PW"]) != md5("123")))
     {
         header("WWW-Authenticate: Basic realm='My realm'");
--- web6/settings.php
+++ web6/settings.php
@@ -3,4 +3,5 @@
     $database_login = "u16428";
     $database_password = "6878969";
     $database_name = "u16428";
-    $connection = mysqli_connect($database_url, $database_login, $database_password, $database_name) or die();?>
+    $connection = mysqli_connect($database_url, $database_login, $database_password, $database_name) or die();
+    print("good");?>
